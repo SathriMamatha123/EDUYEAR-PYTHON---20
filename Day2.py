f90091dafb2fc7de1ced1d5593877c98b294518c
SathriMamatha123 <83662261+SathriMamatha123@users.noreply.github.com>
2021-05-05 09:14:17 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,31 @@
+Name='Stella'
+Marks=90
+Balance =30864.89
+AccNo =13786431991
+
+print("My name is {}, and I got {} Marks in ComputerScience".format(Name,Marks))
+
+print("My Account Number {}, and my Balance is {}" .format(AccNo,Balance))
+
+print("My name is",Name,"and I got" ,Marks, "Marks in ComputerScience")
+
+print("my name is " + Name +"." )
+
+print(f"my name is {Name}, and my AccountNumber is {AccNo}") 
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
