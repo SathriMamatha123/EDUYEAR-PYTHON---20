57aeb449e756d238c2cd0edb14110e1f78e8bc2f
SathriMamatha123 <83662261+SathriMamatha123@users.noreply.github.com>
2021-05-07 12:50:01 +0530

    Add files via upload

---
+++ Day4Prog.py
@@ -0,0 +1,15 @@
+# 1. Use string functions to count the occurrence of 'y' in a word given by user.
+string = input("Enter the String:")
+substring ="y"
+print("The Count is:", string.count(substring))
+
+
+# 2.Take input of a string and print it's even indexed characters
+
+String=input("Enter the String:")
+print(String[::2])
+
+#3.create a program to swap case. Using string functions
+
+Eduyear= input("Enter the string:")
+print(Eduyear.swapcase())
